152ce8d08cc8d6b98640630f7e38bd36cef762f7
tcillard <thomas.cillard@protonmail.com>
2024-04-22 21:35:52 +0200

    fix(infran): remove infra

--- src/movement/application/controller/movement.controller.ts
+++ src/movement/application/controller/movement.controller.ts
@@ -1,6 +1,6 @@
 import { Body, Controller, Inject, Post } from "@nestjs/common"
 import { MovementServiceInterface, MovementServiceToken } from "src/movement/domain/service/movement.service.Interface"
-import { ValidationControllerResponse, ValidationDto } from "src/movement/infra/adapters/controller/validation.dto"
+import { ValidationControllerResponse, ValidationDto } from "src/movement/adapter/controller/validation.dto"
 
 @Controller('movements')
 
